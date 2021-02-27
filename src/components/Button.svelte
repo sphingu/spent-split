0817323ab74c0e6877a1b8668bbda3ae27a4b904
Sumit Hingu <sumit.hingu@einfochips.com>
2021-02-28 00:38:50 +0530

    Added loader style in button

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
+  import { fly } from 'svelte/transition'
   export let type: 'button' | 'submit' | 'reset' = 'button'
   export let kind: 'primary' | 'secondary' | 'default' = 'default'
   export let size: 'default' | 'small' | 'big' = 'default'
   export let disabled: boolean = false
+  export let loading: boolean = true
 
   $: props = {
     type,
-    disabled,
+    disabled: disabled || loading,
     class: ['btn', `btn--${kind}`, `btn--${size}`, $$restProps.class]
       .filter(Boolean)
       .join(' '),
@@ -14,5 +16,8 @@
 </script>
 
 <button {...props} on:click>
+  {#if loading}
+    <span transition:fly={{ x: -20, duration: 200 }} class="spinner" />
+  {/if}
   <slot />
 </button>
